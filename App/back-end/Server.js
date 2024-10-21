d01e460ab866e2f9ef80883a01888e372a821ff5
ABHIGX <149522028+Abhi-GX@users.noreply.github.com>
2024-10-22 01:47:45 +0530

    Update Server.js

--- App/back-end/Server.js
+++ App/back-end/Server.js
@@ -22,6 +22,6 @@ console.log("hello backend");
 // mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 //   .then(() => console.log('MongoDB Connected...'))
 //   .catch(err => console.error('MongoDB connection error:', err));
-
+// console.log("gmat");
 
 app.listen(5000, () => console.log(`Server started on port 5000`));
